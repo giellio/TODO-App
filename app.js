e21cd74e0561556d078ee62f490ca97ada0c25f8
Giellio <gielmichael@hotmail.com>
2024-05-01 13:25:34 +0200

    Fixed aspect ratio being weird - Added div on which it does work

--- app.js
+++ app.js
@@ -42,10 +42,11 @@ function updateTodos () {
 function createTodoElement (todo, todoIndex) {
   const li = document.createElement('li')
   li.className = 'todo'
-  li.innerHTML = `<input type="checkbox" name="todo${todoIndex}" id="todo${todoIndex}" class="custom-checkbox">
+  li.innerHTML = `<input type="checkbox" name="todo${todoIndex}" id="todo${todoIndex}">
+                  <div class="custom-checkbox">
                   <label for="todo${todoIndex}" class="custom-checkbox"><svg fill="var(--secondary-colour)" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24">
                       <path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z" />
-                  </svg></label>
+                  </svg></label></div>
                   <label class="todo-text" for="todo${todoIndex}">${todo.text}</label>
                   <button class="delete-button"><svg xmlns="http://www.w3.org/2000/svg" fill="var(--secondary-text-colour)" height="24" viewBox="0 -960 960 960" width="24">
                       <path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z" />
